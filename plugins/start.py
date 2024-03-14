c96a49ac4534909380c4a75fa4ff88cadb031e38
dashamusic <158588894+dashamusic@users.noreply.github.com>
2024-03-15 01:25:50 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,7 +88,6 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(" ⚡Alok Anime⚡", callback_data = "about"),
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
             ]
